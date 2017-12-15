df76961f7553586ef657e8971cbe3e7b4948e9db
Lucas Faria <lucas10faria@hotmail.com>
2017-12-15 17:49:35 +0000

    Initial Menu Version

--- app/src/main/java/pt/ipp/estg/garagemvirtual/HomeActivity.java
+++ app/src/main/java/pt/ipp/estg/garagemvirtual/HomeActivity.java
@@ -2,6 +2,8 @@ package pt.ipp.estg.garagemvirtual;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,4 +12,14 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        super.onCreateOptionsMenu(menu);
+        MenuInflater mi=this.getMenuInflater();
+        mi.inflate(R.menu.option_submenu,menu);
+
+        return true;
+    }
+
 }
