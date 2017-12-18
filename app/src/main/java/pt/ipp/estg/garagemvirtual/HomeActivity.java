c833c28e5f52d94c69498ad80f260e3b8ee303f2
Lucas de Oliveira Pereira Faria <8120136@estg.ipp.pt>
2017-12-18 18:06:12 +0000

    go to new activity

--- app/src/main/java/pt/ipp/estg/garagemvirtual/HomeActivity.java
+++ app/src/main/java/pt/ipp/estg/garagemvirtual/HomeActivity.java
@@ -1,9 +1,11 @@
 package pt.ipp.estg.garagemvirtual;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -20,6 +22,23 @@ public class HomeActivity extends AppCompatActivity {
         mi.inflate(R.menu.option_submenu,menu);
 
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menu) {
+        // Handle item selection
+        switch (menu.getItemId()) {
+            case R.id.submenu1:
+                return true;
+            case R.id.submenu2:
+                Intent intent = new Intent(this, MapActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.submenu3:
+                return true;
+            default:
+                return super.onOptionsItemSelected(menu);
+        }
     }
 
 }
